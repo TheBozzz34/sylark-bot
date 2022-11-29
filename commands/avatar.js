83f6a70e4204b7b0b1dc8eb02b76a206ad5be7b2
Ethan James <lskywalker416@gmail.com>
2022-11-28 19:47:57 -0700

    allow checking of other users avatars

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,8 +3,20 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('avatar')
-		.setDescription('Replies with your avatar!'),
+		.setDescription('Replies with your avatar!')
+		.addUserOption(option =>
+			option
+				.setName('target')
+				.setDescription('The member whose avatar you want to see')
+				.setRequired(false)),
 	async execute(interaction) {
-		await interaction.reply(`Your avatar: <${interaction.user.displayAvatarURL({ dynamic: true })}>`);
+		if (interaction.options.getUser('target')) {
+			const user = interaction.options.getUser('target');
+			await interaction.reply(`${user.username}'s avatar: ${user.displayAvatarURL({ dynamic: true })}`);
+		}
+		else {
+			await interaction.reply(`Your avatar: ${interaction.user.displayAvatarURL({ dynamic: true })}`);
+		}
+		// await interaction.reply(`Your avatar: <${interaction.user.displayAvatarURL({ dynamic: true })}>`);
 	},
 };
